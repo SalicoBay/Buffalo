0dd1a57d2035fac408db466fd8e14e18e396357a
Salico <salico@Bay.local>
2025-10-05 18:22:10 -0400

    Blunder spotted, more thinking required

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,19 @@
 const std = @import("std");
 const Buffalo = @import("buffalo");
 
-var mem_buffer_1: [1000]u8 = undefined;
-var Allocator: std.heap.FixedBufferAllocator = .init(&mem_buffer_1);
-var a = Allocator.allocator();
-
 pub fn main() !void {
-    // Simple convenience wrapper around Std.Io.Reader/Writers. Instantiates 2 :: [size % 2]u8 buffers.
+    // Simple convenience wrapper around Std.Io.Reader/Writers. Instantiates as [size]u8 buffer.
     // No allocator is needed for the init, as we construct via inline semantics. DO NOT PANIC
     // No other member functions will *ever* allocate additional memory through inline semantics.
     // Any member function that accepts a [~]const u8 will implicitly instantiate a stack-local
     // std.Io.reader.fixed() whose lifetime will naturally end *before* the method return completes.
 
-    var buffalo: Buffalo.Buffalo = .init(100); // <-- Initial capacity of the buffalo
+    var buffalo: Buffalo.Buffalo = .init(3_000_000); // <-- Initial capacity of the buffalo
 
-    buffalo
-        .packFirst(20, "I wanna be the very best, like no one ever was")
-        .inspect()
-        .consumePackPlus(" \n\n")
-        .dropOff()
-        .consumeFirst(20, "I wanna be the very best, like no one ever was")
-        .inspect()
-        .consumeBytesXTimes("\n", 1)
-        .dropOff()
+    try buffalo
+        .pullXTimes("Yahtzee! ", 3_000, "Yahtzee! ")
+        .wait();
+
+    try buffalo
         .rest();
 }
-
-// .consume("The quick brown fox jumped over the lazy dog!") //Have the Buffalo 'eat' the entire string
-// //   This can potentially be an IO event if the string's length is sufficiently large as to exceed
-// //   the capacity of the Buffalo's component buffers.
-// .dropOff() // Have the Buffalo pass whatever it's carrying to Io. Zero out state
-// //
-// .consumeFirst(32, "The quick brown fox jumped over the lazy dog!") // 'Eat' the first x bytes of the string
-// //
-// .dropOff() // All reads are done with a temporary, fixed reader using the Buffalo's buffer as dest
-// //
-// .newPack(100, a) // Discard current state, accept new backing buffers.
-// //
-// .consumeBytesXTimes("\n", 3) //Trying to pull bytes > str.len, will cause Buffalo to drain the entire str
-// //
-// .consumeFirst(1200, "The quick brown fox jumped over the lazy dog!") // Using a fixed buffer allocator works
-// //                                                                   well here. It's basically a ring
-// //                                                                   buffer for our ring buffer!
-// .dropOff() // There's nothing to pass to IO, but internal state is zero'd implicitely.
-// .rest(); // NoOp call chain terminator
--- src/root.zig
+++ src/root.zig
@@ -1,23 +1,11 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-const Reader = struct {
-    buf: []u8,
-    IO: *std.Io.Reader,
-    inline fn init(str: []const u8) Reader {
-        var host = std.Io.Reader.fixed(str);
-        return Reader{
-            .buf = host.buffer,
-            .IO = &host,
-        };
-    }
-};
-
 const Writer = struct {
     buf: []u8,
     IO: *std.Io.Writer,
 
-    inline fn init(comptime bufType: type, buf: *bufType) Writer {
+    inline fn init(buf: []u8) Writer {
         var outFile = std.fs.File.stdout();
         var host = outFile.writer(buf);
         return Writer{
@@ -26,7 +14,7 @@ const Writer = struct {
         };
     }
 
-    inline fn initFileWriter(comptime bufType: type, buf: *bufType, filePath: []const u8) Writer {
+    inline fn initFileWriter(buf: []u8, filePath: []const u8) Writer {
         var outFile = std.fs.cwd().createFile(filePath, .{}) catch {};
         var host = outFile.writer(buf);
         return Writer{
@@ -38,134 +26,96 @@ const Writer = struct {
 
 pub const Buffalo = struct {
     buffer: []u8 = undefined,
-    count: usize = 0,
-    rider: *Writer = undefined,
+    rider: Writer = undefined,
 
-    pub inline fn init(comptime size: usize) Buffalo {
-        var wBuf: [size]u8 = undefined;
-        var swapBuf: [size]u8 = undefined;
-        var writer = Writer.init(@TypeOf(wBuf), &wBuf);
+    pub inline fn init(comptime size: usize, a: std.mem.Allocator) Buffalo {
+        _ = a; // Todo implement use of allocator -- realizing that there's no real benefit 'cheating' here
+        var buf: [size]u8 = undefined;
+        var writer = Writer.init(&buf);
         return Buffalo{
-            .rider = &writer,
-            .buffer = &swapBuf,
+            .rider = writer,
+            .buffer = writer.buf,
         };
     }
 
-    pub fn packThis(self: *Buffalo, str: []const u8, a: std.mem.Allocator) *Buffalo {
-        if (str.len > self.buffer.len) { // Total buffalo capacity is buffalo.pack.len * 2
-            return self.newPack(str.len * 2, a).pack(str);
-        }
-        return self.pack(str);
-    }
-
-    pub fn packFirst(self: *Buffalo, size: usize, str: []const u8) *Buffalo {
-        if (size > self.buffer.len) { // Total buffalo capacity is buffalo.pack.len * 2
-            return self.pack(str[0..self.buffer.len]);
-        }
-        return self.pack(str[0..size]);
-    }
-
-    fn pack(self: *Buffalo, str: []const u8) *Buffalo {
-        self.count += str.len;
-        std.debug.assert(self.buffer.len >= str.len);
-        @memcpy(self.buffer, str);
+    pub fn inspect(self: *Buffalo) *Buffalo {
+        std.debug.print("{s}", .{self.buffer});
         return self;
     }
 
-    pub fn consumePack(self: *Buffalo) *Buffalo {
-        defer self.count = 0;
-        return self.consumeThis(self.buffer[0..self.count]);
-    }
-
-    pub fn consumePackPlus(self: *Buffalo, cs: []const u8) *Buffalo {
-        defer self.count = 0;
-        return self.consumeThis(self.buffer[0..self.count]).consumeBytes(cs);
-    }
-
-    pub fn consumeThis(self: *Buffalo, str: []const u8) *Buffalo {
-        if (str.len > self.buffer.len * 2) { // Total buffalo capacity is buffalo.pack.len * 2
+    pub fn pull(self: *Buffalo, str: []const u8) *Buffalo {
+        if (str.len > (self.buffer.len - self.rider.IO.end)) {
             self.rider.IO.writeAll(str) catch {};
             return self;
         }
-        var reader: Reader = .init(str);
-        while (reader.IO.readSliceShort(self.buffer)) |bytesRead| {
+        var reader = std.Io.Reader.fixed(str);
+        while (reader.readSliceShort(self.buffer[self.rider.IO.end..])) |bytesRead| {
             if (bytesRead == 0) break;
-            if (bytesRead < self.buffer.len and bytesRead > 0) {
-                _ = self.tinyPush(bytesRead);
-                break;
-            }
-            _ = self.push();
+            self.rider.IO.end += bytesRead;
         } else |_| {}
         return self;
     }
 
-    pub fn consumeFirst(self: *Buffalo, count: usize, str: []const u8) *Buffalo {
-        if (count < str.len) {
-            return self.consumeThis(str[0..count]);
-        } else return self.consumeThis(str);
+    pub fn pullFrom(self: *Buffalo, count: usize, str: []const u8) *Buffalo {
+        if (count < str.len - 1) {
+            return self.pull(str[count..]);
+        } else return self.pull(str);
     }
 
-    pub fn consumeByte(self: *Buffalo, c: u8) *Buffalo {
+    pub fn pullLast(self: *Buffalo, count: usize, str: []const u8) *Buffalo {
+        if ((count + 1) < str.len) {
+            return self.pull(str[str.len - count .. str.len]);
+        } else return self.pull(str);
+    }
+
+    pub fn pullFirst(self: *Buffalo, count: usize, str: []const u8) *Buffalo {
+        if ((count + 1) < str.len) {
+            return self.pull(str[0 .. count + 1]);
+        } else return self.pull(str);
+    }
+
+    pub fn pullBetween(self: *Buffalo, l: usize, r: usize, str: []const u8) *Buffalo {
+        if (l > r) {
+            return self.pull(str[r..l]);
+        }
+        if (0 <= l and r <= str.len) {
+            return self.pull(str[l..r]);
+        } else return self.pull(str);
+    }
+
+    pub fn takeByte(self: *Buffalo, c: u8) *Buffalo {
         _ = self.rider.IO.writeByte(c) catch 0;
         return self;
     }
 
-    pub fn consumeBytes(self: *Buffalo, c: []const u8) *Buffalo {
-        for (c) |codepoint| {
-            _ = self.rider.IO.writeByte(codepoint) catch 0;
-        }
-        return self;
-    }
-
-    pub fn consumeBytesXTimes(self: *Buffalo, cs: []const u8, rep: usize) *Buffalo {
+    pub fn pullXTimes(self: *Buffalo, cs: []const u8, rep: usize, sep: ?[]const u8) *Buffalo {
         if (rep > 0) {
             for (0..rep) |_| {
                 for (cs) |c| {
                     _ = self.rider.IO.writeByte(c) catch 0;
                 }
+                if (sep) |s| {
+                    _ = self.pull(s);
+                }
             }
             return self;
         }
-        for (cs[0 .. cs.len - 1]) |c| {
+        for (cs[0 .. cs.len - 1]) |c| { // End loop one cycle early...
             _ = self.rider.IO.writeByte(c) catch 0;
         }
-        return self.consumeByte(cs[cs.len - 1]);
+        return self.takeByte(cs[cs.len - 1]); // ...so we can retain interface fluency
     }
 
-    pub fn inspect(self: *Buffalo) *Buffalo {
-        std.debug.print("---------------<Inspection>---------------\n\n", .{});
-        std.debug.print("Buffalo: start::[`{s}`]::end\n", .{self.buffer[0..self.count]});
-        std.debug.print("\n", .{});
-        std.debug.print("Rider: start::[`{s}`]::end\n", .{self.rider.buf});
-        std.debug.print("\n---------------</Inspection>---------------\n\n", .{});
-        return self;
-    }
-
-    pub fn rest(self: *Buffalo) void {
+    pub fn wait(self: *Buffalo) !void { //Terminate call chain without flushing
         _ = self;
     }
 
-    pub fn newPack(self: *Buffalo, size: usize, packer: std.mem.Allocator) *Buffalo {
-        self.buffer = packer.alloc(u8, @divFloor(size, 2)) catch undefined;
-        self.rider.buf = packer.alloc(u8, @divFloor(size, 2)) catch undefined;
-        self.rider.IO.end = 0;
+    pub fn flush(self: *Buffalo) *Buffalo {
+        _ = self.rider.IO.flush();
         return self;
     }
 
-    pub fn dropOff(self: *Buffalo) *Buffalo {
+    pub fn rest(self: *Buffalo) !void { //Terminate call chain AND flush
         _ = self.rider.IO.flush() catch {};
-        @memset(self.buffer.ptr[0..self.buffer.len], ' ');
-        @memset(self.rider.IO.buffer, ' ');
-        return self;
-    }
-
-    fn tinyPush(self: *Buffalo, offset: usize) *Buffalo {
-        _ = self.rider.IO.write(self.buffer[0..offset]) catch 0;
-        return self;
-    }
-
-    fn push(self: *Buffalo) *Buffalo {
-        _ = self.rider.IO.write(self.buffer) catch 0;
-        return self;
     }
 };
